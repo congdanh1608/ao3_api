569947e0f40e535f4761e0f119e5a4454c79a21e
Trần Công Danh <congdanh1608@gmail.com>
2024-01-04 10:19:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # Trong file main.py
-# from AO3 import app
-from ao3_api import AO3
+from AO3 import app
 
 if __name__ == "__main__":
     import unicorn
 
-    uvicorn.run(AO3, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
