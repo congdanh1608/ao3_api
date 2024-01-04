93f018f3b43cc25071b6fdf9efa3599589fe1088
Trần Công Danh <congdanh1608@gmail.com>
2024-01-04 10:38:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-# Trong file main.py
+# from AO3 import app
+
+# if __name__ == "__main__":
+#     import unicorn
+
+#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+
 from AO3 import app
 
-if __name__ == "__main__":
-    import unicorn
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+@app.get("/works/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
